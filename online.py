5b1ad519b32bbf76b1ed11af853af84f7ca17dd9
umbr 3 <30674647+umbresp@users.noreply.github.com>
2017-11-17 22:33:47 -0600

    Update online.py

--- online.py
+++ online.py
@@ -1,3 +1,21 @@
 import discord
-from discord import commands.ext
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix=!, self_bot=True, description="e")
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    """Pong! Check your response time."""
+    msgtime = ctx.message.timestamp.now()
+    await (await bot.ws.ping())
+    now = datetime.datetime.now()
+    ping = now - msgtime
+    pong = discord.Embed(title='Pong! Response Time:',
+    					 description=str(ping.microseconds / 1000.0) + ' ms',
+                         color=0x00ffff)
+    await bot.say(embed=pong)
+    
+try:
+    bot.run(TOKEN.strip('\"'), bot=False)
+except Exception as e:
+    print(f'\n[ERROR]: \n{e}\n')
